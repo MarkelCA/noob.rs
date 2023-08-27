6d3164a9a841e24db9f7b940726586b2fd08a0e9
MarkelCA <cuestaarribas.markel@proton.me>
2023-08-27 12:58:13 +0200

    Improvements in guess app

--- guess/src/main.rs
+++ guess/src/main.rs
@@ -3,35 +3,39 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let x = rand::thread_rng().gen_range(1..=10);
+    let solution = rand::thread_rng().gen_range(1..=10);
     let stdin = io::stdin();
 
     let mut buffer = String::new();
-    
+
     loop {
         stdin.read_line(&mut buffer)?;
         let input = buffer.trim();
-
-        println!("The string: {}", input);
 
         if input == "exit" {
             break;
         }
 
-        let y = input.parse::<i32>().unwrap(); 
-
-        let result = match x.cmp(&y) {
-            Ordering::Less => "lower",
-            Ordering::Greater => "greater",
-            Ordering::Equal => break,
+        let guess = match input.parse::<i32>() {
+            Ok(x) => x,
+            Err(_) => {
+                println!("Number it's not valid, try again");
+                buffer.clear();
+                continue;
+            }
         };
 
+        let result = match solution.cmp(&guess) {
+            Ordering::Less => "lower",
+            Ordering::Greater => "greater",
+            Ordering::Equal => {
+                println!("Well done!");
+                break;
+            }
+        };
         println!("The number is {}, try again.", result);
-
         buffer.clear();
     }
-
-    println!("Well done!");
 
     Ok(())
 }
