69ac6c2af7160ac923bbe68c77a38c92fa8c2b7c
Markel Cuesta <cuestaarribas.markel@proton.me>
2024-04-06 21:59:23 +0200

    Optimized cat functionality to use a buffer

--- cat/src/main.rs
+++ cat/src/main.rs
@@ -1,3 +1,6 @@
+use std::io::{BufReader, Read, Write};
+use std::fs::File;
+
 use clap::Parser;
 
 /// Simple program to greet a person
@@ -6,6 +9,8 @@ use clap::Parser;
 struct Args {
     files: Vec<String>
 }
+
+const BUFFER_CAPACITY: usize = 1024 * 64;
 
 fn main()-> std::io::Result<()> {
     let args = Args::parse();
@@ -17,12 +22,20 @@ fn main()-> std::io::Result<()> {
     Ok(())
 }
 
-fn print_file(filename: String) -> std::io::Result<()> {
-    let bytes = std::fs::read(filename).unwrap();
-    
-    for bytes in bytes.chunks(50) {
-        print!("{}", String::from_utf8_lossy(bytes));
+fn print_file(filename: String) -> std::io::Result<()>{
+    let f = File::open(filename)?;
 
+    let mut reader = BufReader::with_capacity(BUFFER_CAPACITY, f);
+    let mut buffer = [0; BUFFER_CAPACITY ];
+    let mut stdout = std::io::stdout();
+
+    loop {
+         let n = reader.read(&mut buffer).unwrap();
+         if n > 0 {
+             stdout.write(&buffer[0..n])?;
+         } else {
+             break;
+         }
     }
 
     Ok(())
