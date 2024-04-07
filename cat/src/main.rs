4d2147123bdfa14634492f48afca0edcaa50585a
Markel Cuesta <cuestaarribas.markel@proton.me>
2024-04-07 11:50:18 +0200

    Some refactoring to the cat utility

--- cat/src/main.rs
+++ cat/src/main.rs
@@ -1,9 +1,7 @@
 use std::io::{BufReader, Read, Write};
 use std::fs::File;
-
 use clap::Parser;
 
-/// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
@@ -16,25 +14,30 @@ fn main()-> std::io::Result<()> {
     let args = Args::parse();
 
     for f in args.files {
-        print_file(f)?
+        cat_file(f)?
     }
 
     Ok(())
 }
 
-fn print_file(filename: String) -> std::io::Result<()>{
+fn cat_file(filename: String) -> std::io::Result<()> {
     let f = File::open(filename)?;
-
     let mut reader = BufReader::with_capacity(BUFFER_CAPACITY, f);
-    let mut buffer = [0; BUFFER_CAPACITY ];
+    let mut buffer = [0; BUFFER_CAPACITY];
     let mut stdout = std::io::stdout();
-
+    let mut stderr = std::io::stderr();
     loop {
-         let n = reader.read(&mut buffer).unwrap();
-         if n > 0 {
-             stdout.write(&buffer[0..n])?;
-         } else {
-             break;
+         match reader.read(&mut buffer) {
+             Ok(n) => {
+                 if n > 0 {
+                     stdout.write(&buffer[0..n])?;
+                 } else {
+                     break;
+                 }
+             },
+             Err(err) => {
+                 _ = stderr.write(err.to_string().as_bytes());
+             }
          }
     }
 
