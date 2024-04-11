26ef7c82df345de77b7260fef93673646bb41d55
Markel Cuesta <cuestaarribas.markel@proton.me>
2024-04-11 23:06:47 +0200

    Some progress in the grep functionality
    error handling + bytes buffer + slice searching

--- grep/src/grep.rs
+++ grep/src/grep.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader, Read};
+use std::io::{BufReader, Read};
+use std::io::Write;
 use std::fs;
 use std::path::PathBuf;
 
@@ -22,24 +23,58 @@ fn grep_dir(path: PathBuf, text: &str) {
         if p.is_dir() {
             grep_dir(p, text);
         } else {
-            let x = grep_file(p.display().to_string(), text);
-            x.unwrap()
+
+            match grep_file(p.display().to_string(), text) {
+                Ok(_) => {
+
+                },
+                Err(err) => {
+                    eprintln!("Error reading file {} ({err})", p.display())
+                }
+            }
         }
     }
 }
 
 
+const BUFFER_CAPACITY: usize = 1024 * 64;
 fn grep_file(file_path: String, text: &str) -> std::io::Result<()> {
-    let file = File::open(&file_path)?;
-    let reader = BufReader::new(file);
+    let path = PathBuf::from(file_path);
 
-    let bytes = reader.bytes();
-    for byte in bytes {
-        let byte = byte.unwrap();
-        if byte == text.as_bytes()[0] {
-            println!("{} -> {:?}", file_path, text);
-        }
+    let file = File::open(&path)?;
+
+    // let f = File::open(filename)?;
+    let mut reader = BufReader::with_capacity(BUFFER_CAPACITY, file);
+    let mut buffer = [0; BUFFER_CAPACITY];
+    let mut stdout = std::io::stdout();
+    // let stderr = std::io::stderr();
+
+    loop {
+         match reader.read(&mut buffer) {
+             Ok(n) => {
+                 if n > 0 {
+                     let b = text.as_bytes();
+                     if find_subsequence(&buffer[0..n], b).is_some() {
+                         stdout.write(&buffer[0..n])?;
+                     }
+                     // stdout.write(&buffer[0..n])?;
+                 } else {
+                     break;
+                 }
+             },
+             Err(err) => {
+                 // _ = stderr.write(err.to_string().as_bytes());
+             }
+         }
     }
+    // let bytes = reader.bytes();
+    // for byte in bytes {
+    //     let byte = byte.unwrap();
+    //     if byte == text.as_bytes()[0] {
+    //         println!("{} ", byte);
+    //         // println!("{} -> {:?}", path.display(), text);
+    //     }
+    // }
 
     // for line in reader.lines() {
     //     println!("{} -> {:?}",file_path, line);
@@ -49,4 +84,8 @@ fn grep_file(file_path: String, text: &str) -> std::io::Result<()> {
     //     }
     // }
     Ok(())
+}
+
+fn find_subsequence(haystack: &[u8], needle: &[u8]) -> Option<usize> {
+    haystack.windows(needle.len()).position(|window| window == needle)
 }
---
+++ tmp/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let x = 5;
+
+    let a = &mut x;
+    let b = &xw
+}
